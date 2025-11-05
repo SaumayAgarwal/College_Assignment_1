2446542fd7db9f3aa5947f02dfd6983e0692d414
Saumay Agrawal <saumayagarwal6397@gmail.com>
2025-11-05 15:14:32 +0530

    Update code.java

--- Problem 1/code.java
+++ Problem 1/code.java
@@ -1,1 +1,21 @@
+import java.util.* ;
+import java.io.*; 
+import java.util.ArrayList;
+
+public class Solution 
+{
+    public static void reverseArray(ArrayList<Integer> arr, int m)
+    {
+        // Write your code here.
+        int n=arr.size()-1;
+        m=m+1;
+        while(m<n){
+            int temp=arr.get(m);
+            arr.set(m, arr.get(n));
+            arr.set(n, temp);
+            n--;
+            m++;
+       }
+    }
+}
 
