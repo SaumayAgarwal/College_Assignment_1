34625ff2844a5d97d416c2ccee54f5cb70fbbffe
Saumay Agrawal <saumayagarwal6397@gmail.com>
2025-11-26 23:40:47 +0530

    Create code_1.java

---
+++ Problem 58/code_1.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int climbStairs(int n){
+        int prev1=1;
+        int prev2=0;
+        for(int i=1;i<=n;i++){
+            int var=prev1+prev2;
+            prev2=prev1;
+            prev1=var;
+        }
+        return prev1;
+    }
+}
