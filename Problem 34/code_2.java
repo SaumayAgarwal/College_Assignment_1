cb05537e270fd53446d9b6cbbc85c1767f87746d
Saumay Agrawal <saumayagarwal6397@gmail.com>
2025-11-26 22:45:12 +0530

    Create code_2.java

---
+++ Problem 34/code_2.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        PriorityQueue<int[]> pq=new PriorityQueue<>((a, b) -> b[1]-a[1]);
+        Map<Integer, Integer> map=new HashMap<>();
+        for(int num:nums){
+            map.put(num, map.getOrDefault(num, 0)+1);
+        }
+        for(int key:map.keySet()){
+            pq.add(new int[]{key, map.get(key)});
+        }
+        int[] ans=new int[k];
+        for(int i=0;i<k;i++){
+            ans[i]=pq.poll()[0];
+        }
+        return ans;
+    }
+}
