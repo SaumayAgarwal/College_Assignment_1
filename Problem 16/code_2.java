61c9d45ed4311661d0a212d439b8923c548bcfbc
Saumay Agrawal <saumayagarwal6397@gmail.com>
2025-11-26 18:58:47 +0530

    Update code_2.java

--- Problem 16/code_2.java
+++ Problem 16/code_2.java
@@ -1,45 +1,30 @@
-class Node{
-    int val;
-    Node next;
-    Node prev;
-    Node(int val){
-        this.val=val;
-        this.next=null;
-        this.prev=null;
-    }
-}
-class MinStack {
-    Node head;
-    Node last;
-    List<Integer> list=new ArrayList<>();
-    public MinStack() {
-        head=new Node(0);
-        last=head;
-    }
-    public void push(int val) {
-        Node temp=new Node(val);
-        last.next=temp;
-        temp.prev=last;
-        last=last.next;
-    }
-    
-    public void pop() {
-        if(last==head)return;
-        last=last.prev;
-        last.next=null;
-    }
-    
-    public int top() {
-        return last.val;
-    }
-    
-    public int getMin() {
-        int min=Integer.MAX_VALUE;
-        Node curr=head.next;
-        while(curr!=null){
-            if(min>curr.val)min=curr.val;
-            curr=curr.next;
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> st=new Stack<>();
+        boolean equal;
+        for(int i: asteroids){
+            if(i<0){
+                equal=false;
+                while(!st.isEmpty() && st.peek()>=0){
+                    int j=st.pop();
+                    if(-i==j){
+                        equal=true;
+                        break;
+                    }
+                    if(-i>j)continue;
+                    else {
+                        i=j;
+                        break;
+                    }
+                }
+                if(!equal)st.push(i);
+            }
+            else st.push(i); 
         }
-        return min;
+        int[] ans=new int[st.size()];
+        for(int i=ans.length-1;i>=0;i--){
+            ans[i]=st.pop();
+        }
+        return ans;
     }
 }
