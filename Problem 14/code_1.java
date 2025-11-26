e6ec9e54cfd535c22ea3c699b991ee5bff1b9e84
Saumay Agrawal <saumayagarwal6397@gmail.com>
2025-11-26 18:13:29 +0530

    Create code_1.java

---
+++ Problem 14/code_1.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+class Stack {
+    private static final int STACK_CAPACITY = 101;
+    private char[] stackArray = new char[STACK_CAPACITY];
+    private int topIndex = -1;
+
+    void push(char character) {
+    }
+
+    char pop() {
+    }
+
+    boolean isEmpty() {
+        return topIndex == -1;
+    }
+
+    boolean isFull() {
+        return topIndex >= STACK_CAPACITY - 1;
+    }
+}
+
+class ReverseString {
+    public static void main(String[] args) {
+        String inputString = "Hello, World!";
+        int inputLength = inputString.length();
+
+        Stack charStack = new Stack();
+
+        for (int i = 0; i < inputLength; i++) {
+            char currentChar = inputString.charAt(i);
+            charStack.push(currentChar);
+        }
+
+        StringBuilder reversedString = new StringBuilder();
+        while (!charStack.isEmpty()) {
+            reversedString.append(charStack.pop());
+        }
+
+        System.out.println(reversedString.toString());
+    }
+}
