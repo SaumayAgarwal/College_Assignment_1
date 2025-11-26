cbbd2cb45fec4f3a5e16fc1ebe43f59d973e86a9
Saumay Agrawal <saumayagarwal6397@gmail.com>
2025-11-26 22:16:39 +0530

    Create code_3.java

---
+++ Problem 24/code_3.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean check(TreeNode left, TreeNode right){
+        if(left==null && right==null)return true;
+        if(left==null || right==null)return false;
+        return (left.val==right.val && check(left.left, right.right) && check(left.right, right.left));
+    }
+    public boolean isSymmetric(TreeNode root) {
+        return check(root.left, root.right);
+    }
+}
